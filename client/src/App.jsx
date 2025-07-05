29dceb7bfcee202a543ca4c3bdf6cc94ee0ba4fd
Znarfieeee <watsszap25@gmail.com>
2025-07-05 20:08:13 +0800

    Added tailwindcss, and react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,30 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Landing_page from "./pages/Landing_page"
+import ProtectedRoute from "./components/ProtectedRoute"
+import Layout from "./layout/Layout"
+
 function App() {
-    return <></>
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: (
+                <ProtectedRoute>
+                    <Layout />
+                </ProtectedRoute>
+            ),
+            children: [
+                {
+                    index: true,
+                    element: <Landing_page />,
+                },
+            ],
+        },
+    ])
+    return (
+        <AppProvider>
+            <RouterProvider router={router} />
+        </AppProvider>
+    )
 }
 
 export default App
