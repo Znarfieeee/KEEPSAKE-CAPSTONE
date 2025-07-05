ccb522531f301b72f82675399a0adcf4842abe43
Znarfieeee <watsszap25@gmail.com>
2025-07-05 17:19:49 +0800

    Follow-up

--- server/auth/routes.py
+++ server/auth/routes.py
@@ -4,15 +4,29 @@ import bcrypt
 
 auth_bp = Blueprint('auth', __name__)
 
-@auth_bp.route('/create_user', methods=['GET','POST'])
+@auth_bp.route('/create_user', methods=['POST'])
 def create_user():
     data = request.json
     email = data.get('email')
+    password = data.get('password')
+    firstname = data.get('firstname')
+    lastname = data.get('lastname')
+    role = data.get('role')
     
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     
-    return jsonify({"message": "Account created successfully!", "status":"sucess"}), 201
+    response = supabase.table("USERS").insert({
+        "EMAIL": email,
+        "PASSWORD": hashed_password.decode('utf-8'),
+        "FIRSTNAME": firstname,
+        "LASTNAME": lastname,
+        "ROLE": role
+    }).execute()
+    
+    return jsonify({"message": "Account created successfully!", "status":"success"}), 201
 
-@auth_bp.route('/login', methods=['GET','POST'])
+@auth_bp.route('/login', methods=['POST'])
 def login():
     data = request.json
     email = data.get('email')
@@ -20,7 +34,7 @@ def login():
     result = supabase.auth.sign_in_with_password({"email": email, "password": password})
     if result.get('error'):
         return jsonify({"error": result['error']['message']}), 401
-    return jsonify({"session": result['session']}), 200
+    return jsonify({"message": "Login successful!", "status": "success", "session": result['session']}), 200
 
 
 @auth_bp.route('/logout', methods=['POST'])
