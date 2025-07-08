22d6073dc2c5bd4608a005ac872b0f50ee02590f
Znarfieeee <watsszap25@gmail.com>
2025-07-08 09:36:47 +0800

    Login page - frontend [only]

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Landing_page from "./pages/Landing_page"
 // import ProtectedRoute from "./components/ProtectedRoute"
 import Layout from "./layout/Layout"
+import Login from "./pages/Login"
 
 function App() {
     const router = createBrowserRouter([
@@ -14,6 +15,10 @@ function App() {
                     element: <Landing_page />,
                 },
             ],
+        },
+        {
+            path: "/login",
+            element: <Login />,
         },
     ])
     return <RouterProvider router={router} />
